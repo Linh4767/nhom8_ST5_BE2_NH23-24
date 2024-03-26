3676cb301ecc5a8590bce806db8fbdbbdcda6dee
Linh4767 <ngoclinh24032004@gmail.com>
2024-03-26 16:07:37 +0700

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,7 +22,7 @@
         rel="stylesheet">
     <!-- script
     ================================================== -->
-    <script src="js/modernizr.js"></script>
+    <script src="{{asset('js/modernizr.js')}}"></script>
 </head>
 
 <body data-bs-spy="scroll" data-bs-target="#navbar" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true"
@@ -406,28 +406,10 @@
                                 </div>
                             </div>
                         </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="swiper-pagination position-absolute text-center"></div>
-    </section>
-    <section id="smart-watches" class="product-store padding-large position-relative">
-        <div class="container">
-            <div class="row">
-                <div class="display-header d-flex justify-content-between pb-3">
-                    <h2 class="display-7 text-dark text-uppercase">Smart Watches</h2>
-                    <div class="btn-right">
-                        <a href="shop.html" class="btn btn-medium btn-normal text-uppercase">Go to Shop</a>
-                    </div>
-                </div>
-                <div class="swiper product-watch-swiper">
-                    <div class="swiper-wrapper">
                         <div class="swiper-slide">
                             <div class="product-card position-relative">
                                 <div class="image-holder">
-                                    <img src="images/product-item6.jpg" alt="product-item" class="img-fluid">
+                                    <img src="images/product-item2.jpg" alt="product-item" class="img-fluid">
                                 </div>
                                 <div class="cart-concern position-absolute">
                                     <div class="cart-button d-flex">
@@ -439,16 +421,16 @@
                                 </div>
                                 <div class="card-detail d-flex justify-content-between align-items-baseline pt-3">
                                     <h3 class="card-title text-uppercase">
-                                        <a href="#">Pink watch</a>
+                                        <a href="#">Iphone 11</a>
                                     </h3>
-                                    <span class="item-price text-primary">$870</span>
+                                    <span class="item-price text-primary">$1100</span>
                                 </div>
                             </div>
                         </div>
                         <div class="swiper-slide">
                             <div class="product-card position-relative">
                                 <div class="image-holder">
-                                    <img src="images/product-item7.jpg" alt="product-item" class="img-fluid">
+                                    <img src="images/product-item3.jpg" alt="product-item" class="img-fluid">
                                 </div>
                                 <div class="cart-concern position-absolute">
                                     <div class="cart-button d-flex">
@@ -460,16 +442,16 @@
                                 </div>
                                 <div class="card-detail d-flex justify-content-between align-items-baseline pt-3">
                                     <h3 class="card-title text-uppercase">
-                                        <a href="#">Heavy watch</a>
+                                        <a href="#">Iphone 8</a>
                                     </h3>
-                                    <span class="item-price text-primary">$680</span>
+                                    <span class="item-price text-primary">$780</span>
                                 </div>
                             </div>
                         </div>
                         <div class="swiper-slide">
                             <div class="product-card position-relative">
                                 <div class="image-holder">
-                                    <img src="images/product-item8.jpg" alt="product-item" class="img-fluid">
+                                    <img src="images/product-item4.jpg" alt="product-item" class="img-fluid">
                                 </div>
                                 <div class="cart-concern position-absolute">
                                     <div class="cart-button d-flex">
@@ -481,16 +463,16 @@
                                 </div>
                                 <div class="card-detail d-flex justify-content-between align-items-baseline pt-3">
                                     <h3 class="card-title text-uppercase">
-                                        <a href="#">spotted watch</a>
+                                        <a href="#">Iphone 13</a>
                                     </h3>
-                                    <span class="item-price text-primary">$750</span>
+                                    <span class="item-price text-primary">$1500</span>
                                 </div>
                             </div>
                         </div>
                         <div class="swiper-slide">
                             <div class="product-card position-relative">
                                 <div class="image-holder">
-                                    <img src="images/product-item9.jpg" alt="product-item" class="img-fluid">
+                                    <img src="images/product-item5.jpg" alt="product-item" class="img-fluid">
                                 </div>
                                 <div class="cart-concern position-absolute">
                                     <div class="cart-button d-flex">
@@ -502,30 +484,9 @@
                                 </div>
                                 <div class="card-detail d-flex justify-content-between align-items-baseline pt-3">
                                     <h3 class="card-title text-uppercase">
-                                        <a href="#">black watch</a>
+                                        <a href="#">Iphone 12</a>
                                     </h3>
-                                    <span class="item-price text-primary">$650</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="product-card position-relative">
-                                <div class="image-holder">
-                                    <img src="images/product-item10.jpg" alt="product-item" class="img-fluid">
-                                </div>
-                                <div class="cart-concern position-absolute">
-                                    <div class="cart-button d-flex">
-                                        <a href="#" class="btn btn-medium btn-black">Add to Cart<svg
-                                                class="cart-outline">
-                                                <use xlink:href="#cart-outline"></use>
-                                            </svg></a>
-                                    </div>
-                                </div>
-                                <div class="card-detail d-flex justify-content-between pt-3">
-                                    <h3 class="card-title text-uppercase">
-                                        <a href="#">black watch</a>
-                                    </h3>
-                                    <span class="item-price text-primary">$750</span>
+                                    <span class="item-price text-primary">$1300</span>
                                 </div>
                             </div>
                         </div>
